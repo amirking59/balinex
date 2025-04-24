f8c24372e0d985462382a55f03789e9aa1d6818d
Amir hossein rezaei <58273240+amirking59@users.noreply.github.com>
2025-04-24 13:38:04 +0330

    feat: add coins api

---
+++ app/api/coins/route.ts
@@ -0,0 +1,25 @@
+import BaseApi from "@/lib/api";
+import { Coin } from "@/services/Coin";
+import { NextResponse } from "next/server";
+
+const api = BaseApi.getInstance();
+
+export async function GET() {
+  try {
+    const response = await api.request<Coin[]>("https://api.coingecko.com/api/v3/coins/list", {
+      headers: {
+        "accept": "application/json",
+        "x-cg-demo-api-key": process.env.coin_gecko_api_key!,
+      },
+      next: {
+        revalidate: 60,
+      },
+    });
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
+  }
+}
---
+++ lib/api.ts
@@ -0,0 +1,37 @@
+class BaseApi {
+  private static instance: BaseApi;
+  private baseUrl: string;
+
+  private constructor() {
+    this.baseUrl = "/api";
+  }
+
+  public static getInstance(): BaseApi {
+    if (!BaseApi.instance) {
+      BaseApi.instance = new BaseApi();
+    }
+    return BaseApi.instance;
+  }
+
+  public async request<T>(endpoint: string, options: RequestInit = {}) {
+    const headers = {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    };
+
+    const response = await fetch(endpoint, {
+      ...options,
+      headers,
+      signal: AbortSignal.timeout(15000),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "API error");
+    }
+
+    return response.json() as T;
+  }
+}
+
+export default BaseApi;
