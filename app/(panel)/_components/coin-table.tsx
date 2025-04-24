12be8ce6d67def71419c52b3de79597414f165fc
Amir hossein rezaei <58273240+amirking59@users.noreply.github.com>
2025-04-24 15:55:28 +0330

    feat: add some fields to coin table

--- app/(panel)/_components/coin-table.tsx
+++ app/(panel)/_components/coin-table.tsx
@@ -4,15 +4,32 @@ import { DataTable } from "@/components/data-table";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Coin, useGetCoins } from "@/services/Coin";
 import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
 
 export const columns: ColumnDef<Coin>[] = [
   {
+    size: 100,
     accessorKey: "name",
-    header: "Name",
+    header: "Coin",
+    cell: ({ row }) => {
+      return (
+        <div className="flex gap-2 items-center">
+          <Image src={row.original.image} alt="logo" width={24} height={24} />
+
+          <div className="text-sm font-semibold">
+            {row.original.name} <span className="font-normal text-muted-foreground">{row.original.symbol}</span>
+          </div>
+        </div>
+      );
+    },
   },
   {
-    accessorKey: "id",
-    header: "Id",
+    size: 100,
+    accessorKey: "current_price",
+    header: "Price",
+    cell: ({ row }) => {
+      return <div className="text-sm">$ {row.original.current_price.toLocaleString()}</div>;
+    },
   },
 ];
 
